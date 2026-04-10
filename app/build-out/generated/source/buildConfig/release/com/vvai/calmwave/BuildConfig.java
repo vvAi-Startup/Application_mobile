d579fc8b509b2fa51da3c3e4e284346aacd326ee
Daniel Augusto Mandira <105872910+DanielMandira@users.noreply.github.com>
2026-04-10 18:59:28 -0300

    Feat: Aplica barra de progresso de gravação

---
+++ app/build-out/generated/source/buildConfig/release/com/vvai/calmwave/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.vvai.calmwave;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.vvai.calmwave";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String API_BASE_URL = "https://calm-wave-backend.onrender.com";
+  // Field from default config.
+  public static final String DB_BASE_URL = "https://calm-wave-backend.onrender.com";
+  // Field from default config.
+  public static final String WS_BASE_URL = "wss://calm-wave-backend.onrender.com";
+}
